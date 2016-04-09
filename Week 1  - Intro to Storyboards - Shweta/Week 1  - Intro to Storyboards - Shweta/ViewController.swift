3dbfc51d6cbd868ed48d4f0f88edd10b7b6f4153
JSSSB <shweta.j.chug@outlook.com>
2016-04-09 13:05:37 +1000

    test

---
+++ Week 1  - Intro to Storyboards - Shweta/Week 1  - Intro to Storyboards - Shweta/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Week 1  - Intro to Storyboards - Shweta
+//
+//  Created by Jagmit Chug on 9/04/2016.
+//  Copyright © 2016 Shweta Chug. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Week 1  - Intro to Storyboards - Shweta/Week 1  - Intro to Storyboards - ShwetaTests/Week_1____Intro_to_Storyboards___ShwetaTests.swift
@@ -0,0 +1,36 @@
+//
+//  Week_1____Intro_to_Storyboards___ShwetaTests.swift
+//  Week 1  - Intro to Storyboards - ShwetaTests
+//
+//  Created by Jagmit Chug on 9/04/2016.
+//  Copyright © 2016 Shweta Chug. All rights reserved.
+//
+
+import XCTest
+@testable import Week_1____Intro_to_Storyboards___Shweta
+
+class Week_1____Intro_to_Storyboards___ShwetaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
